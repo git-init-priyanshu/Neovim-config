bf575154aaaffde2fc997f1d53a63c1bcee71a1d
Priyanshu Bartwal <110045644+git-init-priyanshu@users.noreply.github.com>
2024-12-17 20:28:43 +0530

    Configured theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,23 +6,30 @@
 local M = {}
 
 M.base46 = {
-	theme = "monekai",
+  theme = "monekai",
 
-	-- hl_override = {
-	--	Comment = { italic = true },
-	--	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  --	Comment = { italic = true },
+  --	["@comment"] = { italic = true },
+  -- },
   
   changed_themes = {
     monekai = {
       base_16 = { 
-        base08 = "#ffffff", -- ( white )
-        base0A = "#a6e22d", -- ( green )
-        base0B = "#d6bc63", -- ( yellow )
-        base0C = "#f92672", -- ( red )
-      } 
+        base02 = "#5b5c55", -- ( grey ) highlight
+        base08 = "#ffffff", -- ( white ) text
+        base0A = "#a6e22d", -- ( green ) text
+        base0B = "#d6bc63", -- ( yellow ) text
+        base0C = "#f92672", -- ( red ) text
+        base0F = "#ffffff" -- ( white ) commas
+      },
+      base_30 = {
+      	grey = "#808080", -- ( grey ) line number
+      	grey_fg = "#a1a1a1" -- ( grey ) comments
+      }
     }
   }
+
 }
 
 return M
